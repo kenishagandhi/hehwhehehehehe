901e53b82ba4f8caa2d2c2bd2d51eeeadc0e1745
kenishagandhi <117497349+kenishagandhi@users.noreply.github.com>
2022-11-12 19:59:30 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL('BirthdayImage.jpg', function(Img)
+	block_Image_object= Img;
+    block.Image.object. scaletoWidth=(700);
+    block.Image.object. scaleheight=(510);
+    block_Image_object.set({
+top:0
+
+    } )
+	
+}
+
+function playSound(){
+	
+}
